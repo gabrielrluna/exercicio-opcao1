eec526fd481be342c179471deb7c3a04f5a43c48
Gabriel Luna <lunagabri@gmail.com>
2023-01-23 09:40:26 -0300

    Update App.js

--- App.js
+++ App.js
@@ -1,15 +1,26 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, TextInput, SafeAreaView } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  TextInput,
+  SafeAreaView,
+} from "react-native";
 
 export default function App() {
   return (
     <SafeAreaView>
       <StatusBar>
         <TextInput
-          style={estilos.campo}
+          style={estilos.nomeLocal}
           // value={value}
           placeholder="Digite o nome do local"
         />
+
+        <Button style={estilos.botaoFoto} />
+
+        <Button style={estilos.botaoMapa} />
       </StatusBar>
     </SafeAreaView>
   );
